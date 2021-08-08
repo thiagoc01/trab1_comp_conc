65d6c65d68184666ae32607f4dbc080acaf6448b
Thiago Castro <thiago.castro99@hotmail.com>
2021-08-08 14:48:06 -0300

    Adicionada randomização ao gerar as sequências.

--- generateGeneticSequence.c
+++ generateGeneticSequence.c
@@ -9,13 +9,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define NBASES 100000 // numero de caracteres (bases nucleotidicas)
+#define NBASES 10000000
 
 int main(void) {
+    srand(time(NULL));
   FILE *file;
   char basesArray[] = {'A', 'T', 'C', 'G'};
-  int result, i, index, count = 0;
-  char lastElement;
+  int result, i, index;
 
   file = fopen("in.txt", "wt");
 
@@ -24,23 +24,28 @@ int main(void) {
       return 2;
   }
 
+  char last_generated = ' '; // Guarda a última base nitrogenada gerada
+  int counter_repeated_occurences = 0; // Conta o número de bases nitrogenadas iguais seguidas se ocorrer repetição. Máximo de 4 vezes
+
   for (i = 0; i < NBASES; i++) {
-    index = rand() % 4; // criterio do indice pode ser mudado
+    index = rand() % 4;
 
-    if (lastElement == basesArray[index] && count == 3) {
-      while (lastElement == basesArray[index])
-        index = rand() % 4;
-      count = 0;
+    if (last_generated == basesArray[index] && counter_repeated_occurences == 3) // Limite de 4 bases nitrogenadas iguais seguidas
+    {
+        while (last_generated == basesArray[index]) // Continua até gerar algo diferente
+            index = rand() % 4;
+        counter_repeated_occurences = 0; // Reinicia o contador
     }
-    else if (lastElement == basesArray[index])
-      count++;
-    else
-      count = 0;
 
-    lastElement = basesArray[index];
-    result = fputc(lastElement, file);
-					  
-    if (result == EOF)		    
+    else if (last_generated == basesArray[index])
+        counter_repeated_occurences++;
+    else
+      counter_repeated_occurences = 0; // Gerou uma nova base nitrogenada da última e essa não havia chegado no limite
+
+    last_generated = basesArray[index];
+    result = fputc(last_generated, file);
+
+    if (result == EOF)
        printf("Erro na Gravacao\n");
   }
 
